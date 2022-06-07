3b9a0293ead3dd6e92f1c7785cc3507c62eb3707
Mike <mikej.miller440@gmail.com>
2022-06-07 14:04:53 -0500

    converting data is now a utility

---
+++ utils/convertData.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const csv = require('csvtojson');
+
+// convert file to array
+const convertFile = async () => {
+  // read name of file in input dir
+  const inputName = fs.readdirSync('./input')[0];
+
+  const data = await csv().fromFile(`./input/${inputName}`);
+
+  // make data prettier
+  const newData = [];
+  for await (const obj of data) {
+    let { Date, Description, Amount, Balance } = obj;
+    const newObj = {
+      date: Date,
+      description: Description.replace(/  +/g, ' '),
+      amount: Amount.replace('$', ''),
+      balance: Balance.replace(',', '').replace('$', '')
+    };
+
+    newData.push(newObj);
+  }
+
+  // save data into dist folder
+  fs.writeFile('./dist/data.json', JSON.stringify(newData, null, 2), err => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('data.json successfully written in the dist folder');
+    }
+  });
+};
+
+module.exports = convertFile;
