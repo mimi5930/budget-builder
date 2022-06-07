3f1d27c333d9deca54c40034432bb14808c8a5e7
Mike <mikej.miller440@gmail.com>
2022-06-07 13:46:01 -0500

    implemented csv to json conversion

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
-const completeTransactions = require('./input/data.json');
+const fs = require('fs');
+const csv = require('csvtojson');
 
-console.log(completeTransactions[0]);
+// convert file to array
+const convertFile = async () => {
+  // read name of file in input dir
+  const inputName = fs.readdirSync('./input')[0];
+
+  const data = await csv().fromFile(`./input/${inputName}`);
+
+  // make data prettier
+  const newData = [];
+  for await (const obj of data) {
+    let { Date, Description, Amount, Balance } = obj;
+    const newObj = {
+      date: Date,
+      description: Description.replace(/  +/g, ' '),
+      amount: Amount.replace('$', ''),
+      balance: Balance.replace(',', '').replace('$', '')
+    };
+
+    newData.push(newObj);
+  }
+
+  // save data into dist folder
+  fs.writeFile('./dist/data.json', JSON.stringify(newData, null, 2), err => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('data.json written in the dist folder');
+    }
+  });
+};
+
+convertFile();
