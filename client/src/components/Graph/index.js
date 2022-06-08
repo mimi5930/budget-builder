9e5ff4a6960e0507c12a532037384725e3f5bcdd
Mike <mikej.miller440@gmail.com>
2022-06-07 21:13:57 -0500

    added appbar and graphjs

---
+++ client/src/components/Graph/index.js
@@ -0,0 +1,106 @@
+import React, { useRef } from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Filler
+} from 'chart.js';
+import { Line, getElementAtEvent } from 'react-chartjs-2';
+import { Box } from '@mui/material';
+import sampleData from '../../sampledata.json';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Filler
+);
+
+export default function Graph(props) {
+  const chartRef = useRef();
+
+  // organize data
+  var labels = [];
+  var totals = [];
+  var descriptions = [];
+  var amounts = [];
+  sampleData.forEach(transaction => {
+    labels.push(transaction.date);
+    totals.push(transaction.balance);
+    descriptions.push(
+      transaction.description
+        .replace('Point of Sale Withdrawal ', '')
+        .replace('External Withdrawal ', '')
+    );
+    amounts.push(transaction.amount.toFixed(2));
+  });
+
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        data: totals,
+        fill: true,
+        backgroundColor: 'rgba(75,192,192,0.2)',
+        borderColor: 'rgba(75,192,192,1)',
+        tension: 0.2
+      }
+    ]
+  };
+
+  const options = {
+    responsive: true,
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    },
+    plugins: {
+      tooltip: {
+        titleAlign: 'center',
+        bodyAlign: 'center',
+        displayColors: false,
+        callbacks: {
+          label: function (context) {
+            let label = descriptions[context.dataIndex];
+            return label;
+          },
+          afterLabel: function (context) {
+            let afterLabel = amounts[context.dataIndex];
+            return afterLabel;
+          }
+        }
+      }
+    }
+  };
+
+  function clickHandler(event) {
+    const pointIndex = getElementAtEvent(chartRef.current, event)[0]?.index;
+    if (pointIndex) {
+      console.log(pointIndex);
+    }
+  }
+
+  return (
+    <Box
+      sx={{
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: 30,
+        width: '50%'
+      }}
+    >
+      <Line
+        ref={chartRef}
+        data={data}
+        options={options}
+        onClick={clickHandler}
+      />
+    </Box>
+  );
+}
