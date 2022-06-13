46c979cf626814aa9b86c018310ce9ce1f98d684
Mike <mikej.miller440@gmail.com>
2022-06-13 09:39:18 -0500

    set up type selector

--- client/src/components/TypeSelect/index.js
+++ client/src/components/TypeSelect/index.js
@@ -1,9 +1,57 @@
-import React from 'react';
+import { Box, FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+import React, { useState } from 'react';
 
 export default function TypeSelect({ setTransactionData }) {
-	return (
-		<div>
-			<div></div>
-		</div>
-	);
+  const [type, setType] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = async () => {
+    if (!type) return;
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `http://localhost:3001/api/transactions/?type=${type}`
+      );
+      const data = await response.json();
+      setTransactionData(data);
+    } catch (e) {
+      console.log(e);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <Box>
+      <FormControl>
+        <InputLabel id="type">Type</InputLabel>
+        <Select labelId="type" label="Type" value={type}>
+          <MenuItem
+            value="deposit"
+            onClick={event => setType(event.target.getAttribute('data-value'))}
+          >
+            Deposit
+          </MenuItem>
+          <MenuItem
+            value="external withdrawal"
+            onClick={event => setType(event.target.getAttribute('data-value'))}
+          >
+            External Withdrawal
+          </MenuItem>
+          <MenuItem
+            value="point of sale"
+            onClick={event => setType(event.target.getAttribute('data-value'))}
+          >
+            Point of Sale
+          </MenuItem>
+        </Select>
+      </FormControl>
+      <LoadingButton
+        loading={loading}
+        variant="contained"
+        onClick={handleSubmit}
+      >
+        Submit
+      </LoadingButton>
+    </Box>
+  );
 }
